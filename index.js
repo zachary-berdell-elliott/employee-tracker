35cce00fa3f38f30e57dfaea0d221c30cd1e8b71
zachary <zacharyberdell@gmail.com>
2021-11-16 11:09:15 -0700

    Built the first prompt. Added files for different classes.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const inquirer = require("inquirer");
 const mysql2 = require("mysql2");
+
+inquirer
+.prompt([{
+    type: "list",
+    name: "actionSelect",
+    message: "What would you like to do?",
+    choices: ["Add Department", "View Departments", "Add Role", "View Roles", "Add Employee", "View Employees", "Update Employee Role", "Exit Program"]
+}]).then((response) => {
+
+});
