c8353bee608a18de9e2aff1696dcc8d044d69883
Zachary Elliott <zacharyberdell@gmail.com>
2021-11-17 13:41:14 -0700

    Most functions for manipulating the sql have been added. Logic added to unfinished functions.

--- index.js
+++ index.js
@@ -44,31 +44,99 @@ function mainScreen(){
 }
 
 function addDep(){
-
+    //Write inquirer question then INSERT INTO
 }
 
 function viewDeps(){
-
+    connection.query("SELECT * FROM departments", function(err, res){
+        if(err) throw err;
+        console.table(res);
+        mainScreen();
+    })
 }
 
 function addRole(){
-
+    //SELECT from departments
+    //Ask question what is the title of the new role and what is the salary
 }
 
 function viewRoles(){
-
+    connection.query("SELECT * FROM roles", function(err, res){
+        if(err) throw err;
+        console.table(res);
+        mainScreen();
+    })
 }
 
-function addEmp(){
 
+function addEmp(){
+    connection.query("SELECT * FROM roles", function(err, res){
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "firstName",
+                message: "What is the employees first name",
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: "What is the employees last name?"
+            },
+            {
+/*Will be similar to department id in add role function*/            name: "roleId",
+            message: "What is role of the new employee",
+            type: "list",
+            choices: res.map( role => role.title)
+        },
+]).then((response) => {
+        const employeeRole = res.find(role => role.title === response.roleId);
+        connection.query("INSERT INTO employees SET ?", {
+            first_name: response.firstName,
+            last_name: response.lastName,
+            role_id: employeeRole.id
+        },
+        
+        function(){
+            console.log("The employee has been added");
+            mainScreen();
+        })
+    })
+    })
 }
 
 function viewEmps(){
-
+    connection.query("SELECT * FROM employees", function(err, res){
+        if(err) throw err;
+        console.table(res);
+        mainScreen();
+    })
 }
+
 
 function updateEmp(){
-
+    connection.query("select * FROM employees", function(err, res){
+        if (err) throw err;
+        inquirer.prompt([{
+            type: "list",
+            name: "employeeToUpdate",
+            message: "What employee would you like to update?",
+            choices: res.map(employee => employee.first_name + " " + employee.last_name)
+        }]).then((response) => {
+            const selectedEmployee = res.find(employee => employee.first_name + " " + employee.last_name === response.employeeToUpdate);
+            connection.query("SELECT * FROM ROLES", (err, res) => {
+                if (err) throw err;
+                inquirer.prompt([{
+                    type: "list",
+                    name: "newRole",
+                    message: "What would you like to change the employees role to?",
+                    choices: res.map(role => role.title)
+                }]).then((response) => {
+                    const selectedRole = res.find(role => role.title === response.newRole);
+                    connection.query("UPDATE employees SET role_id = ? WHERE id = ?", [selectedRole.id, selectedEmployee.id]);
+                    mainScreen();
+                })
+            })
+        })
+    })
 }
-
-mainScreen();
