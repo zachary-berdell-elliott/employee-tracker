6d4eb357c010f85dc11e8bd330f5ce72bac167bc
zachary <zacharyberdell@gmail.com>
2021-11-17 10:13:50 -0700

    Built out the logic to cycle through the responses in the initial selection.

--- index.js
+++ index.js
@@ -8,5 +8,29 @@ inquirer
     message: "What would you like to do?",
     choices: ["Add Department", "View Departments", "Add Role", "View Roles", "Add Employee", "View Employees", "Update Employee Role", "Exit Program"]
 }]).then((response) => {
+    if(response.actionSelect == "Add Department"){
+        //Code for adding a department
+    }
+    else if (response.actionSelect == "View Departments"){
+        //Code for viewing a department
+    }
+    else if (response.actionSelect == "Add Role"){
+        //Code for adding a role
+    }
+    else if (response.actionSelect == "View Roles"){
+        //Code for viewing roles
+    }
+    else if (response.actionSelect == "Add Employee"){
+        //Code for adding an employee
+    }
+    else if (response.actionSelect == "View Employees"){
+        //Code for viewing employees
+    }
+    else if (response.actionSelect == "Update Employee Role"){
 
+    }
+    else {
+        console.log("Come back later.");
+        return;
+    }
 });
