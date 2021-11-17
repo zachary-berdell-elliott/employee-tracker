96ac7fd7e423dfc89da9510557df5ac9fb4acbb7
Zachary Elliott <zacharyberdell@gmail.com>
2021-11-17 12:08:06 -0700

    Functions for editing and viewing the sql have been added.

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
 const inquirer = require("inquirer");
 const mysql2 = require("mysql2");
 
-inquirer
-.prompt([{
-    type: "list",
-    name: "actionSelect",
-    message: "What would you like to do?",
-    choices: ["Add Department", "View Departments", "Add Role", "View Roles", "Add Employee", "View Employees", "Update Employee Role", "Exit Program"]
-}]).then((response) => {
-    if(response.actionSelect == "Add Department"){
-        //Code for adding a department
-    }
-    else if (response.actionSelect == "View Departments"){
-        //Code for viewing a department
-    }
-    else if (response.actionSelect == "Add Role"){
-        //Code for adding a role
-    }
-    else if (response.actionSelect == "View Roles"){
-        //Code for viewing roles
-    }
-    else if (response.actionSelect == "Add Employee"){
-        //Code for adding an employee
-    }
-    else if (response.actionSelect == "View Employees"){
-        //Code for viewing employees
-    }
-    else if (response.actionSelect == "Update Employee Role"){
+function mainScreen(){
+    inquirer
+    .prompt([{
+        type: "list",
+        name: "actionSelect",
+        message: "What would you like to do?",
+        choices: ["Add Department", "View Departments", "Add Role", "View Roles", "Add Employee", "View Employees", "Update Employee Role", "Exit Program"]
+    }]).then((response) => {
+        if(response.actionSelect == "Add Department"){
+            addDep();
+        }
+        else if (response.actionSelect == "View Departments"){
+            viewDeps();
+        }
+        else if (response.actionSelect == "Add Role"){
+            addRole();
+        }
+        else if (response.actionSelect == "View Roles"){
+            viewRoles();
+        }
+        else if (response.actionSelect == "Add Employee"){
+            addEmp();
+        }
+        else if (response.actionSelect == "View Employees"){
+            viewEmps();
+        }
+        else if (response.actionSelect == "Update Employee Role"){
+            updateEmp();
+        }
+        else {
+            console.log("Come back later.");
+            return;
+        }
+    });
+}
 
-    }
-    else {
-        console.log("Come back later.");
-        return;
-    }
-});
+function addDep(){
+
+}
+
+function viewDeps(){
+
+}
+
+function addRole(){
+
+}
+
+function viewRoles(){
+
+}
+
+function addEmp(){
+
+}
+
+function viewEmps(){
+
+}
+
+function updateEmp(){
+    
+}
+
+mainScreen();
